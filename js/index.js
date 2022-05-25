5de9d65b2fb20bf2940733a20d8fccc34e5e2cee
rubyrubyrubin <99642954+rubyrubyrubin@users.noreply.github.com>
2022-05-25 10:26:49 +0200

    test

--- js/index.js
+++ js/index.js
@@ -3,6 +3,24 @@ const vid = document.querySelector(".video-player video");
 const btn = document.querySelector(".video-player-button");
 vid.playbackRate = 6; // Debug only
 
-function togglePlay() {}
+function togglePlay() {
+    if (vid.paused) {
+vid.play()
+vidContainer.classList.add("playing")
+btn.textContent = "Pause"
+} else {
+    vid.pause()
+    btn.textContent = "Play"
+    vidContainer.classList.remove("playing")
+}
+}
+
+function videoEnded() {
+    if (vidContainer.classList.contains("playing")) {
+        vidContainer.classList.remove("playing")
+    }
+    btn.textContent = "Play"
+}
 
 btn.addEventListener("click", togglePlay);
+vid.addEventListener("ended", videoEnded);
